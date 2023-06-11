1baeb1abeeb7ff964dc2ca897ef9c2d659ca306d
Maxwell11 <maksikbestbest@gmail.com>
2023-06-11 19:27:14 +0300

    dz number6 done

---
+++ number6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (Convert.ToBoolean(a%2)){
+    Console.WriteLine("Нечетное");
+}else{
+    Console.WriteLine("Четное");
+}

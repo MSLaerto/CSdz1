2f365562abb18a5ee5e36905e90356d1aa0df9cc
Maxwell11 <maksikbestbest@gmail.com>
2023-06-11 19:22:30 +0300

    dz number 4 done

---
+++ number4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число : ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число : ");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.Write("max : ");
+if (a<b){
+    if(b<c){
+        Console.WriteLine(c);    
+    }else{
+        Console.WriteLine(b);
+    }
+}else if(a<c){
+    Console.WriteLine(c);
+}else{
+    Console.WriteLine(a);
+}
